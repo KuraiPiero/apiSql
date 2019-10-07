a4952fffb067872090cabcc88a9714d3d62e7dba
villegas-06 <villegas4006@gmail.com>
2019-10-07 14:20:29 -0500

    1

---
+++ server.js
@@ -0,0 +1,26 @@
+//necessary npm middleware packages 
+var express = require("express");
+var bodyParser = require("body-parser");
+var session = require("express-session");
+
+// Setting up port
+var PORT = process.env.PORT || 3000;
+
+//configuring middleware 
+//needed to read through our public folder
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false })); //For body parser
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+
+//test if our server is working fine
+app.get('/', function(req, res) {    
+       res.send('Welcome to Passport with Sequelize and without HandleBars');
+});
+
+//this will listen to and show all activities on our terminal to 
+//let us know what is happening in our app
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
